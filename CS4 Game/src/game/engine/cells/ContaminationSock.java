11babbba6e2e191988e9cad1795f56c5d59c6f67
ranyrafik <rany.rafik@gmail.com>
2026-04-22 20:51:33 +0200

    Update ContaminationSock.java

--- CS4 Game/src/game/engine/cells/ContaminationSock.java
+++ CS4 Game/src/game/engine/cells/ContaminationSock.java
@@ -2,10 +2,8 @@ package game.engine.cells;
 
 import game.engine.Constants;
 import game.engine.interfaces.CanisterModifier;
-import game.engine.monsters.Dynamo;
 import game.engine.monsters.Monster;
-import game.engine.monsters.MultiTasker;
-import game.engine.monsters.Schemer;
+
 
 public class ContaminationSock extends TransportCell implements CanisterModifier {
 
@@ -15,31 +13,13 @@ public class ContaminationSock extends TransportCell implements CanisterModifier
 
 	@Override
 	public void modifyCanisterEnergy(Monster monster, int canisterValue) {
-
-			if (canisterValue < 0 && monster.isShielded()) {
-				monster.setShielded(false);
-				return;
-			}
-			int newCanisterValue = canisterValue;
-			if (canisterValue != 0) {
-				if (monster instanceof Dynamo) {
-					newCanisterValue = newCanisterValue * 2;
-				} else if (monster instanceof MultiTasker) {
-					newCanisterValue = newCanisterValue + Constants.MULTITASKER_BONUS;
-				} else if (monster instanceof Schemer) {
-					newCanisterValue = newCanisterValue + Constants.SCHEMER_STEAL;
-				}
-			}
-			monster.setEnergy(monster.getEnergy() + newCanisterValue);
-
+			monster.alterEnergy(canisterValue);
 	}
 
 	
 	public void transport(Monster monster){
         super.transport(monster);		
-		
-
-			modifyCanisterEnergy(monster, -Constants.SLIP_PENALTY);
+		modifyCanisterEnergy(monster, -Constants.SLIP_PENALTY);
 			
   }
 }
